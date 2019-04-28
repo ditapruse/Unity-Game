222d43e8b938af6bc25e15c877df556aedbfe79c
Dita <pruse.dita@gmail.com>
2019-04-28 10:31:27 +0300

    Adding new scene with gun and gunshots

---
+++ Assets/Scripts/GunFire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour //Rename "Gunfire" to what your script name is
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            AudioSource gunsound = GetComponent<AudioSource>();
+            gunsound.Play();
+            GetComponent<Animation>().Play("Gunshot"); //Rename to what your animation is called
+        }
+
+    }
+}
